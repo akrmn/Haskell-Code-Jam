1426c26a1d3b00a5ade47427abdecca87d5c9353
Moisés Ackerman <akrmn@users.noreply.github.com>
2016-04-16 16:59:56 -0430

    Round 1a

---
+++ lastword.hs
@@ -0,0 +1,15 @@
+lastword letters = foldl aux "" letters
+  where
+    aux "" letter = letter:[]
+    aux acc@(l:_) letter
+      | letter < l = acc ++ (letter:[])
+      | otherwise = letter:acc
+
+doIt :: (Integer, [Char]) -> [Char]
+doIt (i, s) = "Case #" ++ (show i) ++ ": " ++ (lastword s)
+
+main :: IO ()
+main = do
+  tests <- getLine
+  contents <- getContents
+  mapM_ (putStrLn . doIt) $ zip [1..] (take (read tests) (lines contents))
